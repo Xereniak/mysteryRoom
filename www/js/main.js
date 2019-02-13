b872ba4a1b998f44f0022748b62e6835df5b969e
West <west2@uleth.ca>
2019-02-12 17:38:02 -0700

    First commit

--- www/js/main.js
+++ www/js/main.js
@@ -2,18 +2,15 @@ var app = new Framework7({
     //    INITIALIZE APP
     root: '#app',
 
-    //    PANEL RULES
-    panel: {
-        swipe: 'both',
-        swipeActiveArea: 50,
-    },
-    //    PAGE RULES
     routes: [
         {
-            path: '/page2/',
-            url: 'pages/page2.html'
+            path: '/main/',
+            url: 'pages/main.html'
         }
-    ],
+    ]
 });
 
 var mainView = app.views.create('.view-main');
+var swiper = app.swiper.create('.swiper-container', {
+    
+})
