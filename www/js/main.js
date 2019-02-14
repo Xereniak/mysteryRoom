f6c4f996e15c3009714deff8695bdc77599a8f62
West <west2@uleth.ca>
2019-02-14 13:12:27 -0700

    Images added, game image refresh system in place

--- www/js/main.js
+++ www/js/main.js
@@ -22,3 +22,17 @@ $$(document).on('page:init', '.page[data-name="/main/"]', function (e) {
     });
     console.log("we tried");
 });
+
+var img1 = '../img/IMG.main.1.covered.png'
+var img2 = '../img/IMG.main.2.blank.png'
+var img3 = '../img/IMG.main.3.blank.png'
+var img4 = '../img/IMG.main.4.png'
+
+$(document).on('click', function () {
+    $('.img1').css('background-image', `url("${img1}")`);
+    $('.img2').css('background-image', `url("${img2}")`);
+    $('.img3').css('background-image', `url("${img3}")`);
+    $('.img4').css('background-image', `url("${img4}")`);
+});
+
+console.log(`url("${img1}")`);
